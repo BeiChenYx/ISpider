28146f44159b368ce8388504d59e0431a36cb8d3
Utek <yangx@utek.com>
2018-08-28 15:02:30 +0800

    解决home未定义bug

--- ILoveIHomeSlave/ILoveIHomeSlave/spiders/bj.py
+++ ILoveIHomeSlave/ILoveIHomeSlave/spiders/bj.py
@@ -22,23 +22,25 @@ class BjSpider(RedisSpider):
         print('=='*30)
         print('bjurl: ', response.url)
         print('=='*30)
-        try:
-            item = IloveihomeslaveItem()
-            txt_xpath = './div[@class="houseList_list_txt fl"]'
-            name_xpath = txt_xpath + '/div[@class="txt1 style"]/h3/a/text()'
-            covered_xpath = txt_xpath + '/div[@class="style"]//span/text()'
-            addr_xpath = txt_xpath + '/div[@class="style address"]//span/text()'
-            time_xpath = txt_xpath + '/div[@class="style"][2]//span/text()'
-            type_xpath = txt_xpath + '/div[@class="title"]//span/text()'
-            price_xpath = ('./div[@class="price fontS16"]/text() | '
-                            './div[@class="price fontS16"]/i/text()')
+        homes = response.selector.xpath('//div[@class="houseList_list"]')
+        for home in homes:
+            try:
+                item = IloveihomeslaveItem()
+                txt_xpath = './div[@class="houseList_list_txt fl"]'
+                name_xpath = txt_xpath + '/div[@class="txt1 style"]/h3/a/text()'
+                covered_xpath = txt_xpath + '/div[@class="style"]//span/text()'
+                addr_xpath = txt_xpath + '/div[@class="style address"]//span/text()'
+                time_xpath = txt_xpath + '/div[@class="style"][2]//span/text()'
+                type_xpath = txt_xpath + '/div[@class="title"]//span/text()'
+                price_xpath = ('./div[@class="price fontS16"]/text() | '
+                                './div[@class="price fontS16"]/i/text()')
 
-            item['name'] = home.xpath(name_xpath).extract_first().strip()
-            item['covered'] = ' '.join([covered.strip() for covered in home.xpath(covered_xpath).extract()])
-            item['addr'] = ' '.join([addr.strip() for addr in home.xpath(addr_xpath).extract()])
-            item['time'] = ' '.join([time.strip() for time in home.xpath(time_xpath).extract()])
-            item['type_home'] = ' '.join([ty.strip() for ty in home.xpath(type_xpath).extract()])
-            item['price'] = ' '.join([price.strip() for price in home.xpath(price_xpath).extract()])
-            yield item
-        except Exception as err:
-            print(str(err))
+                item['name'] = home.xpath(name_xpath).extract_first().strip()
+                item['covered'] = ' '.join([covered.strip() for covered in home.xpath(covered_xpath).extract()])
+                item['addr'] = ' '.join([addr.strip() for addr in home.xpath(addr_xpath).extract()])
+                item['time'] = ' '.join([time.strip() for time in home.xpath(time_xpath).extract()])
+                item['type_home'] = ' '.join([ty.strip() for ty in home.xpath(type_xpath).extract()])
+                item['price'] = ' '.join([price.strip() for price in home.xpath(price_xpath).extract()])
+                yield item
+            except Exception as err:
+                print(str(err))
