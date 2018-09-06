47f0cf178d39abf20d108e97416926e430387ce5
unknown <yangx@utek.com>
2018-09-06 17:27:56 +0800

    完成了ZHSpider的二级类别的获取

--- ZHSpider/spiders/common.py
+++ ZHSpider/spiders/common.py
@@ -1,6 +1,7 @@
 """
 公用信息
 """
+import requests
 
 # 访问的请求头
 header = {
@@ -12,15 +13,33 @@ header = {
 # 域名
 domain_name = 'https://www.zhihu.com'
 
-def get(url, json=False):
+
+def get(url, isjson=False):
     """
     获取url的页面
     """
     rst = requests.get(url, headers=header)
     print('rst.status_code: ', rst.status_code)
     if rst.status_code == 200:
-        if json:
-            return rst.json
+        if isjson:
+            return rst.json()
+        else:
+            return rst.text
+    return ''
+
+def post(url, formdata, isjson=False):
+    """
+    post方式访问
+    :url 需要访问的地址
+    :formdata 需要提交的数据, 字典格式
+    """
+    # print('url: ', url)
+    # print('formdata: ', formdata)
+    rst = requests.post(url, data=formdata,headers=header )
+    print('rst.status_code: ', rst.status_code)
+    if rst.status_code == 200:
+        if isjson:
+            return rst.json()
         else:
             return rst.text
     return ''
--- ZHSpider/spiders/second_type.py
+++ ZHSpider/spiders/second_type.py
@@ -1,17 +1,70 @@
 """
 获取所有二级类别信息
 """
-from common import request
+import json
+import re
+import time
+
+import common
 
 
-def get_second_type(url):
+def save_result(info):
+    """
+    保存二级类别
+    """
+    with open('secondType.txt', 'a', encoding='utf-8') as fi:
+        for line in info:
+            print(line)
+            fi.write(line+'/hot\n')
+
+def parse_result(data):
+    """
+    提取二级类别的地址
+    """
+    urls = list()
+    for msg in data:
+        url = re.findall('<a target="_blank" href="(.*?)">', msg)
+        urls.append(url[0])
+    return urls
+
+def get_second_type(data_id, offset):
     """
     获取二级类别的地址
     """
-    pass
+    url = 'https://www.zhihu.com/node/TopicsPlazzaListV2'
+    params = '{"topic_id": %d,"offset": %d,"hash_id": ""}'
+    postData = {'method': 'next', 'params': params % (int(data_id), int(offset))}
+    rst = common.post(url, isjson=True, formdata=postData)
+    if rst:
+        return rst['msg']
+    else:
+        return ''
+
+def get_data_id(path):
+    """
+    获取二级类别的topic_id
+    """
+    data = dict()
+    with open(path, 'r', encoding='utf-8') as fi:
+        info = json.load(fi)
+        data.update(info)
+    for info in data['info']:
+        yield info['data-id']
 
 def main():
-    pass
+    topic_ids = get_data_id('./firstType.json')
+    for topic_id in topic_ids:
+        offset = 0
+        print('topic_id strart: ', topic_id)
+        while True:
+            msg = get_second_type(topic_id, offset)
+            if not len(msg):
+                break
+            offset += 20
+            urls = parse_result(msg)
+            save_result(urls)
+            time.sleep(3)
+
 
 if __name__ == '__main__':
     main()
