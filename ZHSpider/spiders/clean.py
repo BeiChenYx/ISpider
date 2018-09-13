c82bb3f53de727a1c00506c7c4cb06daa1ae4b43
yangxing <yangxing2297@163.com>
2018-09-13 23:17:14 +0800

    修改了任务文件和清理文件ZHSpider

--- ZHSpider/spiders/clean.py
+++ ZHSpider/spiders/clean.py
@@ -3,8 +3,18 @@
 """
 import os
 
+def clear_dir(path):
+    """
+    清空指定目录下的文件和指定的目录
+    """
+    if os.path.exists(path):
+        files = os.listdir(path)
+        for fi in files:
+            os.remove(path + '/' + fi)
+        print('delete %s file' % (path + '/' + fi))
+        os.rmdir(path)
+        print('delete %s dir' % path)
+
 if __name__ == "__main__":
-    files = os.listdir('./doc')
-    for fi in files:
-        os.remove('./doc/' + fi)
-        print('delete %s file' % ('./doc/' + fi))
+    clear_dir('./doc')
+    clear_dir('./__pycache__')
--- ZHSpider/spiders/main.py
+++ ZHSpider/spiders/main.py
@@ -1,6 +1,8 @@
 """
 主文件：用来启动所有任务
 """
+import os
+
 import first_type
 import second_type
 import artical_url
@@ -8,6 +10,10 @@ import artical_info
 import question_info
 
 if __name__ == "__main__":
+    if not os.path.exists('./doc'):
+        os.mkdir('./doc')
+        print('创建./doc文件夹')
+
     first_type.main()
     second_type.main()
     artical_url.main()
