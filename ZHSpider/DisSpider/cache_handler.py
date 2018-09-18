be6c0cee6face17267b0308a49909d58ec2a3c88
yangxing <yangxing2297@163.com>
2018-09-18 21:45:23 +0800

    完成了redis操作

--- ZHSpider/DisSpider/cache_handler.py
+++ ZHSpider/DisSpider/cache_handler.py
@@ -9,7 +9,7 @@ class RedisHandler(object):
     处理Redis操作的类
     连接，断开，写入，读取的封装
     """
-    def __init__(self):
+    def __init__(self, host='localhost', port='6379', password=''):
         """
         队列定义:
             1. 一级类别结果存储队列：
@@ -26,14 +26,14 @@ class RedisHandler(object):
                 zh:questioninfo
 
         """
-        self._host = 'localhost'
-        self._port = 6379
-        self._password = ''
+        self._host = host
+        self._port = port
+        self._password = password
         self.iredis = redis.Redis(host=self._host, 
                                   port=self._port, 
                                   decode_responses=True)
 
-        self.one_topic = 'zh:onetopic'
+        self.onetopic = 'zh:onetopic'
         self.twotopic = 'zh:twotopic'
         self.articalurl = 'zh:articalurl'
         self.questionurl = 'zh:questionurl'
@@ -44,17 +44,54 @@ class RedisHandler(object):
         # redis对象在离开作用域会自动析构
         # del self.iredis
 
-    def _set_queue(data, queue_name):
+    def _set_queue(self, data, queue_name):
         """
         将数据写进Redis队列中去
         :data  需要写入的数据
         :queue_name 需要写入的队列名称
         """
-        pass
+        self.lpush(self, queue_name, data)
 
-    def _get_queue(queue_name):
+    def _get_queue(self, queue_name):
         """
         从指定的Redis队列中读取数据
         :queue_name 需要读取的队列名字
         """
-        pass
+        return self.rpop(queue_name)
+
+    def push_onetopic(self, data):
+        self._set_queue(data, self.onetopic)
+
+    def get_onetopic(self)
+        return self._get_queue(self.onetopic)
+
+    def push_twotopic(self, data):
+        self._set_queue(data, self.twotopic)
+
+    def get_twotopic(self)
+        return self._get_queue(self.twotopic)
+
+    def push_articalurl(self, data):
+        self._set_queue(data, self.articalurl)
+
+    def get_articalurl(self)
+        return self._get_queue(self.articalurl)
+
+    def push_articalinfo(self, data):
+        self._set_queue(data, self.articalinfo)
+
+    def get_articalinfo(self)
+        return self._get_queue(self.articalinfo)
+
+    def push_questionurl(self, data):
+        self._set_queue(data, self.questionurl)
+
+    def get_questionurl(self)
+        return self._get_queue(self.questionurl)
+
+    def push_questioninfo(self, data):
+        self._set_queue(data, self.questioninfo)
+
+    def get_questioninfo(self)
+        return self._get_queue(self.questioninfo)
+
